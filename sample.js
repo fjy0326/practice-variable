26ea9a86e687f9146f7e04e84c4a1900e081d066
fjy0326 <bungakukai2010@gmail.com>
2025-02-09 13:17:17 +0000

    20250209update2

--- sample.js
+++ sample.js
@@ -1,10 +1,3 @@
-let alertString;
+let promptStr = prompt('何か好きな文字を入力してください')
 
-alertString = addString("WebCamp")
-
-alert(alertString);
-
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+alert(promptStr);
